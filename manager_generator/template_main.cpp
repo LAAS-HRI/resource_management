8d9057673d7dc9b0791630949184860c394710cb
Guillaume Sarthou <gsarthou@laas.fr>
2019-03-07 17:00:26 +0100

    [ADD] publisher functions to tenplate main for code generation

--- manager_generator/template_main.cpp
+++ manager_generator/template_main.cpp
@@ -9,37 +9,49 @@
 
 class ${class_name} : public ResourceManager<${project_name}::CoordinationSignal
 !!for msg_name in message_names
-      , ${{project_name}}::{msg_name}
+      ,${{project_name}}::{msg_name}
 !!end
-  >
+>
 {
 public:
     ${class_name}(const ros::NodeHandlePtr &nh):
         ResourceManager (std::move(nh),{${reactive_input_names_cs}})
-    {}
+    {
+!!for data_type in messages_types_zip
+        MessageWrapper<{data_type[2]}>::registerPublishFunction(std::bind(&${{class_name}}::publish{data_type[0]}Msg,this,std::placeholders::_1));
+!!end
+    }
 
 private:
     std::map<std::string,std::shared_ptr<MessageAbstraction>> stateFromMsg(const ${project_name}::CoordinationSignal &msg) override;
     std::vector<std::tuple<std::string,std::string,resource_management::EndCondition>>
     transitionFromMsg(const ${project_name}::CoordinationSignal &msg) override;
+
+!!for data_type in messages_types_zip
+    void publish{data_type[0]}Msg({data_type[2]} msg);
+!!end
 };
 
 std::map<std::string,std::shared_ptr<MessageAbstraction>> ${class_name}::stateFromMsg(const ${project_name}::CoordinationSignal &msg)
 {
     std::map<std::string,std::shared_ptr<MessageAbstraction>> states;
 !!for data_type in messages_types_zip
+
     for(auto x : msg.states_{data_type[0]}){{
         auto wrap = states[x.header.id] = std::make_shared<MessageWrapper<{data_type[2]}>>(x.data);
         wrap->setPriority(static_cast<importance_priority_t>(msg.header.priority.value));
     }}
 !!end
+
     return states;
 }
+
 std::vector<std::tuple<std::string,std::string,resource_management::EndCondition>>
 ${class_name}::transitionFromMsg(const ${project_name}::CoordinationSignal &msg)
 {
     std::vector<std::tuple<std::string,std::string,resource_management::EndCondition>> transitions;
 !!for data_type in message_names
+
     for(auto x : msg.states_{data_type}){{
         for(auto t : x.header.transitions){{
             transitions.push_back(
@@ -53,6 +65,13 @@ ${class_name}::transitionFromMsg(const ${project_name}::CoordinationSignal &msg)
     return transitions;
 }
 
+!!for data_type in messages_types_zip
+void ${{class_name}}::publish{data_type[0]}Msg({data_type[2]} msg)
+{{
+  // Put you own publishing function here
+}}
+
+!!end
 int main(int argc, char *argv[]){
     ros::init(argc,argv,"${project_name}");
     ros::NodeHandlePtr nh(new ros::NodeHandle("~"));
