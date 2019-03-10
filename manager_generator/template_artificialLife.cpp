fd879db5d00bf4c6fcff45c0b743e115f4bd79b2
Guillaume Sarthou <gsarthou@laas.fr>
2019-03-10 01:09:31 +0100

    [UPDATE] user information for artificial life class creation

--- manager_generator/template_artificialLife.cpp
+++ manager_generator/template_artificialLife.cpp
@@ -3,13 +3,52 @@
 ${project_name}ArtificialLife::${project_name}ArtificialLife(std::shared_ptr<ReactiveBuffer> buffer) :
         ArtificialLife(100 /* you can change the artficial life frame rate here*/, buffer)
 {
+  // set an initial value in the artificial life buffer
+  // if you do not do that that resource will not start in artificial life mode
 
+  // Example:
+
+  // 1 - Wrap your data with one of your types:
+!!for data_type in messages_types_zip
+  // auto wrapped_{data_type[0]}_data = std::make_shared<MessageWrapper<{data_type[2]}>>(data);
+!!end
+  //
+  // 2 - Set the useless priority to your wrapped data:
+!!for data_type in messages_types_zip
+  // wrapped_{data_type[0]}_data->setPriority(useless);
+!!end
+  //
+  // 3 - Insert your wrapped data into the rtificial life buffer:
+!!for data_type in messages_types_zip
+  // _buffer->setData(wrapped_{data_type[0]}_data);
+!!end
 }
 
 void ${project_name}ArtificialLife::init()
 {
   // Put our own initialisation function here
   // It will be called each time a new artificial life cycle begins
+
+  // Set an initial value in the artificial life buffer
+  // if you do not do that, at each new cycle, the resource
+  // will start with the previous artificial life value
+
+  // Example:
+
+  // 1 - Wrap your data with one of your types:
+!!for data_type in messages_types_zip
+  // auto wrapped_{data_type[0]}_data = std::make_shared<MessageWrapper<{data_type[2]}>>(data);
+!!end
+  //
+  // 2 - Set the useless priority to your wrapped data:
+!!for data_type in messages_types_zip
+  // wrapped_{data_type[0]}_data->setPriority(useless);
+!!end
+  //
+  // 3 - Insert your wrapped data into the rtificial life buffer:
+!!for data_type in messages_types_zip
+  // _buffer->setData(wrapped_{data_type[0]}_data);
+!!end
 }
 
 void ${project_name}ArtificialLife::inLoop()
