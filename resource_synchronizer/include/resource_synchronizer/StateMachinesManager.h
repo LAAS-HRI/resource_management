301b72b51e5cdd074226201f0664b7123d37c5cd
Guillaume Sarthou <gsarthou@laas.fr>
2019-05-17 09:44:16 +0200

    [UPDATE][resource_synchronizer] choose SM to run with preemption and run them

--- resource_synchronizer/include/resource_synchronizer/StateMachinesManager.h
+++ resource_synchronizer/include/resource_synchronizer/StateMachinesManager.h
@@ -29,6 +29,7 @@ public:
 
     done_.resize(state_machines_holders_.size());
     ids_.resize(state_machines_holders_.size());
+    preempt_.resize(state_machines_holders_.size());
 
     while(run_ && ros::ok())
     {
@@ -38,58 +39,61 @@ public:
       {
         for(size_t i = 0; i < ids_.size(); i++)
         {
+
+          //get state machine to run
           int st_id = -1;
           if(ids_[i].size() && !done_[i])
             st_id = ids_[i][0];
           else
             done_[i] = true;
 
-          if(st_id != -1)
+          if(st_id != -1) // if at least one state machine to run for machine i
           {
-            bool to_take = true;
+            bool to_take = true; // if this id can be took
 
-            if(state_machines_holders_[i]->canReplace(st_id))
+            if(preempt_[i]) // don't concider the running machine because need to be preempted
             {
-              for(size_t j = 0; j < ids_.size(); j++)
+              to_take = isSelectable(i, st_id);
+            }
+            else if(state_machines_holders_[i]->canReplace(st_id)) // if machine st_id can preempt_ the one running
+            {
+              to_take = isSelectable(i, st_id);
+              if(to_take && (state_machines_holders_[i]->isRunning() != -1))
               {
-                if(i != j)
-                {
-                  auto it = std::find(ids_[j].begin(), ids_[j].end(), st_id);
-                  if (it != ids_[j].end() && it != ids_[j].begin())
-                    to_take = false;
-                  else if((it != ids_[j].end()) && (!state_machines_holders_[j]->canReplace(st_id)))
-                    to_take = false;
-                }
+                preempt_[i] = true;
+                reinit();
+                continue;
+                // we set this state machime preemptable but do not affect it for the momemnt
               }
             }
-            else
+            else // the one running is more important
               to_take = false;
 
             if(to_take)
-            {
-              for(size_t j = 0; j < ids_.size(); j++)
-              {
-                auto it = std::find(ids_[j].begin(), ids_[j].end(), st_id);
-                if (it != ids_[j].end())
-                  done_[j] = true;
-              }
-
-              clean();
-            }
+              select(st_id);
             else // remove state machine not executable for this run
-            {
-              for(size_t j = 0; j < ids_.size(); j++)
-              {
-                auto it = std::find(ids_[j].begin(), ids_[j].end(), st_id);
-                if (it != ids_[j].end())
-                  ids_[j].erase(it);
-              }
-            }
+              remove(st_id);
           }
         }
       }
 
       // ids to run are in ids_
+      for(size_t i = 0; i < ids_.size(); i++)
+      {
+        if(ids_[i].size())
+        {
+          int running_id = state_machines_holders_[i]->isRunning();
+          if(running_id != ids_[i][0])
+            continue;
+          else if(running_id == -1)
+            state_machines_holders_[i]->send(ids_[i][0]);
+          else
+          {
+            state_machines_holders_[i]->cancel();
+            state_machines_holders_[i]->send(ids_[i][0]);
+          }
+        }
+      }
 
       r.sleep();
     }
@@ -106,6 +110,7 @@ private:
 
   std::vector<bool> done_;
   std::vector<std::vector<int> > ids_;
+  std::vector<bool> preempt_;
 
   void init()
   {
@@ -113,7 +118,18 @@ private:
     {
       ids_[i] = state_machines_holders_[i]->getIdsPerPriorities();
       done_[i] = false;
+      preempt_[i] = false;
     }
+  }
+
+  void reinit()
+  {
+    for(size_t i = 0; i < state_machines_holders_.size(); i++)
+    {
+      ids_[i] = state_machines_holders_[i]->getIdsPerPriorities();
+      done_[i] = false;
+    }
+    cleanPreempted();
   }
 
   bool isDone()
@@ -145,6 +161,71 @@ private:
         }
       }
     }
+  }
+
+  void cleanPreempted()
+  {
+    for(size_t i = 0; i < ids_.size(); i++)
+    {
+      if(preempt_[i])
+      {
+        int preempt_id = state_machines_holders_[i]->isRunning();
+        if(preempt_id != -1)
+        {
+          for(size_t j = 0; j < ids_.size(); j++)
+          {
+            if(state_machines_holders_[j]->isOneRunning(preempt_id))
+              preempt_[j] = true;
+
+            auto it = std::find(ids_[j].begin(), ids_[j].end(), preempt_id);
+            if(it != ids_[j].end())
+              ids_[j].erase(it);
+          }
+        }
+      }
+    }
+  }
+
+  void select(int id)
+  {
+    for(size_t j = 0; j < ids_.size(); j++)
+    {
+      auto it = std::find(ids_[j].begin(), ids_[j].end(), id);
+      if (it != ids_[j].end())
+        done_[j] = true;
+    }
+
+    clean();
+  }
+
+  void remove(int id)
+  {
+    for(size_t j = 0; j < ids_.size(); j++)
+    {
+      auto it = std::find(ids_[j].begin(), ids_[j].end(), id);
+      if (it != ids_[j].end())
+        ids_[j].erase(it);
+    }
+  }
+
+  bool isSelectable(size_t owner, int id)
+  {
+    bool res = true;
+
+    for(size_t j = 0; j < ids_.size(); j++)
+    {
+      if(j != owner)
+      {
+        auto it = std::find(ids_[j].begin(), ids_[j].end(), id);
+        if (it != ids_[j].end() && it != ids_[j].begin()) // if present but not a position 0
+          res = false;
+        else if((it != ids_[j].end()) && (!state_machines_holders_[j]->canReplace(id)) && !preempt_[j])
+        // if at position 0 and can not preempt the running one (not preemtable and not allready preempted)
+          res = false;
+      }
+    }
+
+    return res;
   }
 };
 
